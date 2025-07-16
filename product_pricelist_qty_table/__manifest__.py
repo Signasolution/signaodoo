39b461abcc4d639188c4eb4608ea848e30351a35
jltrompf <jltrompf@free.fr>
2025-07-16 23:55:25 +0200

    Add files via upload

---
+++ product_pricelist_qty_table/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Product Pricelist Qty Table",
+    "version": "1.0",
+    "depends": ["website_sale", "product"],
+    "category": "Website",
+    "summary": "Display quantity-based price breaks on website product pages",
+    "data": [
+        "views/product_template_inherit.xml",
+        "views/product_template_form_inherit.xml"
+    ],
+    "installable": True,
+    "auto_install": False
+}
