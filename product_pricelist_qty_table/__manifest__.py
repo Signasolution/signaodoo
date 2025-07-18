ff6d1613624f7ac295768f2bd61678fe6502051f
jltrompf <jltrompf@free.fr>
2025-07-18 16:02:26 +0200

    Update __manifest__.py

--- product_pricelist_qty_table/__manifest__.py
+++ product_pricelist_qty_table/__manifest__.py
@@ -3,7 +3,7 @@
     "version": "1.0",
     "depends": ["website_sale", "product"],
     "category": "Website",
-    "summary": "Display quantity-based price breaks on website product pages",
+    "summary": "Affiche un tableau avec la liste des prix dégressifs, sur le site web et dans le backend",
     "data": [
         "views/product_template_inherit.xml",
         "views/product_template_form_inherit.xml"
