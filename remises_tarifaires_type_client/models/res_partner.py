39f3dac264711b1b041a439e49658e75608848c1
jltrompf <jltrompf@free.fr>
2025-07-26 22:22:20 +0200

    Delete remises_tarifaires_type_client directory

--- remises_tarifaires_type_client/models/res_partner.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    type_client_id = fields.Many2one('res.partner.type', string="Type de client")
--- remises_tarifaires_type_client/models/sale_order_line.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, api
-
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
-
-    @api.onchange('order_id.partner_id')
-    def _onchange_partner_discount(self):
-        if self.order_id.partner_id.type_client_id:
-            self.discount = self.order_id.partner_id.type_client_id.discount_rate
