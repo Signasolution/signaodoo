14c9e6fb8c69c7c98dfb3ce5bc3dfd810d9d5664
jltrompf <jltrompf@free.fr>
2025-09-17 08:17:25 +0200

    Delete product_product_sheet directory

--- product_product_sheet/models/product_template.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    def action_print_product_sheet(self):
-        self.ensure_one()
-        # Appelle le rapport QWeb par son XMLID et imprime la fiche pour ce produit
-        return self.env.ref("product_product_sheet.action_report_product_sheet").report_action(self)
