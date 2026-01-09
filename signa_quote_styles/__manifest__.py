f6a3fcea626605cd611dac4af4bdb8e4aa218b57
jltrompf <jltrompf@free.fr>
2026-01-09 13:08:01 +0100

    Add files via upload

--- signa_quote_styles/__manifest__.py
+++ signa_quote_styles/__manifest__.py
@@ -1,2 +1,15 @@
 # -*- coding: utf-8 -*-
-{'name': 'Signa - Styles de devis par client', 'version': '18.0.1.0.0', 'category': 'Sales', 'summary': 'Choix automatique du header de devis selon res.partner.x_studio_style_de_devis', 'license': 'LGPL-3', 'author': 'Signa', 'depends': ['sale', 'web'], 'data': ['report/signa_quote_styles.xml'], 'installable': True, 'application': False}
+{
+    "name": "Signa - Styles de devis par client (Odoo 18)",
+    "version": "18.0.1.0.0",
+    "category": "Sales",
+    "summary": "Change le header du devis selon res.partner.x_studio_style_de_devis (4 marques).",
+    "author": "Signa",
+    "license": "LGPL-3",
+    "depends": ["sale", "web"],
+    "data": [
+        "report/signa_quote_styles.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
