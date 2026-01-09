098895cdbf82b41adf1a106ace6ce90af1dfaa9e
jltrompf <jltrompf@free.fr>
2026-01-09 12:57:12 +0100

    Delete signa_quote_styles directory

--- signa_quote_styles/__manifest__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-{'name': 'Signa - Styles de devis par client', 'version': '18.0.1.0.0', 'category': 'Sales', 'summary': 'Choix automatique du header de devis selon res.partner.x_studio_style_de_devis', 'license': 'LGPL-3', 'author': 'Signa', 'depends': ['sale', 'web'], 'data': ['report/signa_quote_styles.xml'], 'installable': True, 'application': False}
