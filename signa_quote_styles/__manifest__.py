4ecb8d377490a84dc9fb7126a9a763fd8a6bb8fc
jltrompf <jltrompf@free.fr>
2026-01-09 10:36:51 +0100

    Add files via upload

--- signa_quote_styles/__manifest__.py
+++ signa_quote_styles/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Signa - Styles de devis par client",
-    "version": "18.0.1.0.17",
+    "version": "18.0.1.0.18",
     "category": "Sales",
     "summary": "Impression et envoi automatique du bon style de devis selon le champ client x_studio_style_de_devis",
     "author": "Jean-Louis",
