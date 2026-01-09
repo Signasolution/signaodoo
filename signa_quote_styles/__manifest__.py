bf92915b35212a3211071b8eb747c3c89e31d1be
jltrompf <jltrompf@free.fr>
2026-01-09 11:06:33 +0100

    Add files via upload

---
+++ signa_quote_styles/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Signa - Styles de devis par client",
+    "version": "18.0.1.0.0",
+    "category": "Sales",
+    "summary": "Choix automatique du layout de devis selon res.partner.x_studio_style_de_devis",
+    "depends": [
+        "sale",
+        "web"
+    ],
+    "data": [
+        "report/signa_saleorder_layouts.xml"
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False
+}
